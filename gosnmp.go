67df1ad28b64fbf288c9125bbe05e19a11ef9ada
Sonia Hamilton <sonia@snowfrog.net>
2014-08-15 17:25:40 +1000

    tidy SET sanity check

--- gosnmp.go
+++ gosnmp.go
@@ -172,10 +172,9 @@ func (x *GoSNMP) Set(pdus []SnmpPDU) (result *SnmpPacket, err error) {
 	if len(pdus) != 1 {
 		return nil, fmt.Errorf("gosnmp currently only supports SNMP SETs for one oid")
 	}
-	//fmt.Println("pduType:", pdus[0].Type)
-	// if pdus[0].Type != Integer || pdus[0].Type != OctetString {
-	// 	return nil, fmt.Errorf("ERR:gosnmp currently only supports SNMP SETs for Integers")
-	// }
+	if pdus[0].Type != Integer || pdus[0].Type != OctetString {
+		return nil, fmt.Errorf("ERR:gosnmp currently only supports SNMP SETs for Integers and OctetStrings")
+	}
 	// build up SnmpPacket
 	packetOut := &SnmpPacket{
 		Community:  x.Community,
